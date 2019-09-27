936866dcfd32def51d32068d554341f9b016d4c2
Ero Osamuyi <eroosamuyi11451@gmail.com>
2019-09-27 00:33:46 -0700

    remote

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,9 @@
 const path = require("path")
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
-const pathholder = path.join(__dirname, '../public');
-app.use(express.static(pathholder))
+const publicpath = path.join(__dirname, '../public');
+app.use(express.static(publicpath))
 
 app.listen(port, () => console.log(`App listening on port ${port}!`)) 

25830642719b53ab84b3beb713cdaf3ad7c3a242
Ero Osamuyi <eroosamuyi11451@gmail.com>
2019-09-27 02:15:14 -0700

    socket added to to the app

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,18 @@
 const path = require("path")
+const http = require("http")
 const express = require('express')
+const soceketIO = require("socket.io")
 const app = express()
+const server = http.createServer(app)
 const port = process.env.PORT || 3000
 
 const publicpath = path.join(__dirname, '../public');
 app.use(express.static(publicpath))
-
-app.listen(port, () => console.log(`App listening on port ${port}!`)) 
+const io = soceketIO(server)
+io.on('connection', (socket:any) => {
+    console.log(`new user connected ${socket}`)
+})
+io.on('dicconnection', (socket:any) => {
+    console.log(`new user disconnected ${socket}`)
+})
+server.listen(port, () => console.log(`App listening on port ${port}!`)) 

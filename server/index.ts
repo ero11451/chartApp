eb7aeb94095444525bb80fb148d664f95daea224
Ero Osamuyi <eroosamuyi11451@gmail.com>
2019-09-28 09:27:30 -0700

    new

--- server/index.ts
+++ server/index.ts
@@ -6,24 +6,24 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
+const server = http.createServer(app)
+// const io = soceketIO(server)
+// io.on('connection', (socket:any) => {
+// // new change made to the file
+//    io.on("createMessage",(message:any)=>{
+//          socket.emit("newmessage",
+//          { 
+//              from:message.from,
+//              to:message.to,
+//              message:message.message
+//          })
+//     })
+  
+// })
+// io.on('dicconnection', (socket:any) => {
+//     console.log(`new user disconnected `)
+// })
 const publicpath = path.join(__dirname, '../public');
 app.use(express.static(publicpath))
 
-const server = http.createServer(app)
-const io = soceketIO(server)
-io.on('connection', (socket:any) => {
-// new change made to the file
-   socket.on("createMessage",(message:any)=>{
-         socket.emit("newmessage",
-         { 
-             from:message.from,
-             to:message.to,
-             message:message.message
-         })
-    })
-  
-})
-io.on('dicconnection', (socket:any) => {
-    console.log(`new user disconnected `)
-})
 server.listen(port, () => console.log(`App listening on port ${port}!`)) 
